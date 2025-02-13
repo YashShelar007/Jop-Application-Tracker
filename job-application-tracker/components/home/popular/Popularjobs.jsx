de60dd608c9672e5443b499e27e88f6ff8891501
Yash Shelar <87018353+YashShelar007@users.noreply.github.com>
2025-02-13 04:23:06 +0000

    add: Home screen done

--- job-application-tracker/components/home/popular/Popularjobs.jsx
+++ job-application-tracker/components/home/popular/Popularjobs.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from "react";
+import { useRouter } from "expo-router";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 
-import styles from './popularjobs.style'
+import styles from "./popularjobs.style";
 
 const Popularjobs = () => {
-  return (
-    <View>
-      <Text>Popularjobs</Text>
-    </View>
-  )
-}
+  const router = useRouter();
 
-export default Popularjobs
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs;
